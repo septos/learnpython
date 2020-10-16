4628379c03f1f8f45d6db8854415bde824fb572c
septo <mistersepto@gmail.com>
2020-10-16 19:39:22 +0700

    upload flie py

---
+++ manipulating string and commenting.py
@@ -0,0 +1,24 @@
+#1
+print("This is a long statement. This is a long statement."
+      "This is a long statement. This is a long statement. ")
+
+#2
+pesan = "hi"
+print((pesan+"\n")*10) #\n disebut escape character
+
+#3
+print("""
+  ____       _                       _     ____        _                    
+ / ___|  ___| | __ _ _ __ ___   __ _| |_  |  _ \  __ _| |_ __ _ _ __   __ _ 
+ \___ \ / _ \ |/ _` | '_ ` _ \ / _` | __| | | | |/ _` | __/ _` | '_ \ / _` |
+  ___) |  __/ | (_| | | | | | | (_| | |_  | |_| | (_| | |_ (_| | | | | (_| |
+ |____/ \___|_|\__,_|_| |_| |_|\__,_|\__| |____/ \__,_|\__\__,_|_| |_|\__, |
+                                                                      |___/ 
+
+""")
+
+
+#4
+print(len(pesan))
+print('Apple\'s amazing')
+print(r"C:\User\Spartan")
