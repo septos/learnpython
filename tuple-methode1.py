e98abc55709c473148ac7fda5573238157e6e8ea
septo-termux <mistersepto@gmail.com>
2020-10-19 15:23:49 +0700

    tambahan2

---
+++ tuple-methode1.py
@@ -0,0 +1,15 @@
+breakfast = ('butter','chese','bread','milk')
+print(breakfast[0:2])
+
+
+motorcycle = ('suzuki','honda','yamaha')
+print(motorcycle)
+
+
+#concatenation
+gabungan = breakfast+motorcycle
+print(gabungan)
+
+
+del breakfast
+print(breakfast)
