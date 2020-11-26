8cdb5bd0b1377704dcc166cfb41db3eb81d3871f
Septo <mistersepto@gmail.com>
2020-11-26 12:45:16 +0700

    Class Progate
    Menimpa methode yang ada di induk diganti pemakaian methode yang ada di anak, kemudian penambahan argument di Food

--- progate python/food.py
+++ progate python/food.py
@@ -1,6 +1,13 @@
 from menu_item import MenuItem
 
 class Food(MenuItem):
+    # Definisikan method __init__ 
+    def __init__(self, name, price, calorie_count):
+        self.name = name
+        self.price = price
+        self.calorie_count = calorie_count
+    
+    
     def info(self):
         return self.name + ': $' + str(self.price) + ' (' + str(self.calorie_count) + 'kkal)'
     
